ee073f13ecc94982bc6e439e87390c714d73897a
DavidMartinLively <112995684+DavidMartinLively@users.noreply.github.com>
2022-09-27 19:43:56 -0700

    Add files via upload

---
+++ assignment 1 103/script.js
@@ -0,0 +1,44 @@
+console.log("This is the console");
+let studentName= "David";
+let studentAge =28;
+let studentEthnicity="White";
+let studentMaritalStatus="Married";
+let studentChildren= 0;
+let studentHousehold=2;
+let studentMilitaryExperience="Navy";
+let studentYearServed ="4";
+let studentYearsOfProgrammingExperience="1";
+let studentFavoriteColor= "purple";
+let studentFavoriteFood= "Pizza";
+let studentHobby ="Skateboarding";
+let studentHowManyPets =2;
+let studentHairColor ="Black";
+let isActive = true;
+let graduationYear =2023;
+let selectedMajor = "Full Stack Web Developer";
+let jobGoal = "Quality Assurance Automator";
+let priorExperience= "QA class";
+let educationExperience ="BA Bio";
+
+
+document.write("<p> Name: " + studentName + "</p>");
+document.write("<p> Age: " + studentAge + "</p>");
+document.write("<p> Active: " + isActive + "</p>");
+document.write("<p> Ethnicity : " +studentEthnicity  + "</p>")
+document.write("<p> Marital Status : " + studentMaritalStatus  + "</p>")
+document.write("<p> Children : " + studentChildren  + "</p>")
+document.write("<p> Household size : " + studentHousehold  + "</p>")
+document.write("<p> Military Experience: " + studentMilitaryExperience  + "</p>")
+document.write("<p> Years Served : " + studentYearServed  + "</p>")
+document.write("<p> Years of programming experience : " + studentYearsOfProgrammingExperience  + "</p>")
+document.write("<p> Favorite color : " + studentFavoriteColor + "</p>")
+document.write("<p> Favorite food : " + studentFavoriteFood + "</p>")
+document.write("<p> Hobby : " + studentHobby  + "</p>")
+document.write("<p> How many pets  : " + studentHowManyPets  + "</p>")
+document.write("<p> Hair color : " + studentHairColor  + "</p>")
+document.write("<p> Active : " + isActive  + "</p>")
+document.write("<p> Graduation year : " + graduationYear  + "</p>")
+document.write("<p> Major : " + selectedMajor  + "</p>")
+document.write("<p> Job goal: " + jobGoal + "</p>")
+document.write("<p> Prior experience : " + priorExperience + "</p>")
+document.write("<p> Education experience: " + educationExperience +"</p>")
